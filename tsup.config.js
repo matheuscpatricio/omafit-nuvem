5f833701d55f2e13c7be0cbe14f2a20ffdc2ba88
matheuscpatricio <mpatricio621@gmail.com>
2026-03-23 12:17:31 -0300

    $

--- tsup.config.js
+++ tsup.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/main.tsx", "src/admin.tsx", "src/home.ts"],
   format: ["esm"],
+  platform: "browser",
   target: "esnext",
   clean: true,
   minify: true,
   bundle: true,
-  noExternal: ["@tiendanube/nexo"],
+  noExternal: [/.*/],
   sourcemap: false,
   splitting: false,
   skipNodeModulesBundle: false,
@@ -15,6 +16,7 @@ export default defineConfig({
     options.alias = {
       "@tiendanube/nube-sdk-jsx/dist/jsx-runtime": "@tiendanube/nube-sdk-jsx/jsx-runtime",
     };
+    options.packages = "bundle";
   },
   outExtension: ({ options }) => ({
     js: options.minify ? ".min.js" : ".js"
